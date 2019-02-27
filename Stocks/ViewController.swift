33a02e70870a64914e99b3a8f123a45bc9f58656
Virtual Machine <virtualmachine@MacBook-Pro-de-Virtual.local>
2019-02-27 15:04:35 -0300

    Initial Commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Virtual Machine on 27/02/19.
+//  Copyright © 2019 Virtual Machine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
